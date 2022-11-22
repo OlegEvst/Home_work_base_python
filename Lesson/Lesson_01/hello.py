93f08a978b8ce0abba2d6e3573ff90fe16b0b8e4
oleg_evstifeev <evstifeev96@yandex.ru>
2022-11-22 21:56:25 +0300

    Рабочее пространство для python

---
+++ Lesson/Lesson_01/hello.py
@@ -0,0 +1,35 @@
+#a = 123
+#b = 1.23
+#s = "hello world"
+#print(a,b,s)
+#print(a,' - ',b, '-', s)
+#print('{1} - {2} - {0}'.format(a, b, s))
+
+#Логические
+#f = False
+#print(f)
+
+#Списки
+#list = [1,'hello',4]
+#print(list)
+
+#a = 2
+#b = 8
+
+#c=a//b
+#print(c)
+
+# a = int(input('a ='))
+# b = int(input('b ='))
+
+# if a > b:
+#     print(a)
+# elif b > a:
+#     print(b)
+# else: 
+#     print('Числа равны')
+
+list = range(10)
+for i in list:
+    print(i)
+
