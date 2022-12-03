7e3123a44f1494471412ed52de772b09301bbcf4
oleg_evstifeev <evstifeev96@yandex.ru>
2022-12-03 14:08:55 +0300

    Лекция 2, чтение и создание файлов

---
+++ Lesson/Lesson_02/task_01.py
@@ -0,0 +1,17 @@
+# colors = ['red', 'green', 'blue']
+# data = open('file.txt', 'a')
+# data.writelines(colors)
+# data.write('LINE 2\n')
+# data.write('LINE 3\n')
+# data.close()
+
+# with open('D:\Рабочий стол\GB\Python основы\Lesson\Lesson_02\ file.txt', 'w') as data:
+#     data.write('LINE 2\n')
+#     data.write('LINE 3\n')
+#     data.writelines('LINE 3\n')
+
+# path = 'D:\Рабочий стол\GB\Python основы\Lesson\Lesson_02\ file.txt' 
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
