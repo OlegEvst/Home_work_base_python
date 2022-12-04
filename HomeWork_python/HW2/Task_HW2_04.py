2ecb3da5aa2cba17583354b49d87dcd4b99845e3
oleg_evstifeev <evstifeev96@yandex.ru>
2022-12-04 13:18:35 +0300

    Добавили решение задачи на рандомное число

---
+++ HomeWork_python/HW2/Task_HW2_04.py
@@ -0,0 +1,35 @@
+#Составьте алгоритм нахождения случайного числа без использования библиотеки random.
+
+import datetime
+import math
+# Импортируем 2 библиотеки 
+def get_second():
+    second = datetime.datetime.today().second
+    return second
+# Функция отдаёт секунды с реальном времени
+def get_range(second_to_day):
+    value = math.ceil(math.exp(second_to_day)) % 1000
+    return value
+# Функция отдаёт число, math cell откругляет до билижайшего целого числа полученного путём
+# экспонирования сенуд и берем остаток от полученного числа
+
+def random_print(random_set,value):
+    for i in range(value):
+        random_set.add(str(i))
+    for j in random_set:
+        print(int(j))
+        break
+# Функция печаетет первое число из заполненного множества и мы получаем рандомное число
+# без закономерности
+
+result = get_range(get_second())
+random_print(set(),result)
+
+
+
+
+
+
+
+
+    
