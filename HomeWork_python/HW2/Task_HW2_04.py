d1daaea98bdca8b5c46a4e7a66ffd74a5036a16c
oleg_evstifeev <evstifeev96@yandex.ru>
2022-12-05 21:16:15 +0300

    Обновили решение задачи 4

--- HomeWork_python/HW2/Task_HW2_04.py
+++ HomeWork_python/HW2/Task_HW2_04.py
@@ -8,7 +8,7 @@ def get_second():
     return second
 # Функция отдаёт секунды с реальном времени
 def get_range(second_to_day):
-    value = math.ceil(math.exp(second_to_day)) % 1000
+    value = math.ceil(math.exp(second_to_day) % 1000) 
     return value
 # Функция отдаёт число, math cell откругляет до билижайшего целого числа полученного путём
 # экспонирования сенуд и берем остаток от полученного числа
@@ -24,9 +24,6 @@ def random_print(random_set,value):
 
 result = get_range(get_second())
 random_print(set(),result)
-
-
-
 
 
 
